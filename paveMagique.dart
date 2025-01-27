4fccb861d8d7f3362dde52658db881c48a63a821
unknown <elishamedenoukoume@gmail.com>
2025-01-27 01:23:01 -1100

    Fonction pour calculer le volume du pavé

---
+++ paveMagique.dart
@@ -0,0 +1,10 @@
+void main() {
+  print(calculVolumePave (5,6,7));
+   
+}
+
+
+double calculVolumePave (double longueur, double largeur, double hauteur) {
+  return longueur*largeur*hauteur;
+  
+  }
