0cadf9ae056519e0268712febf6b170d3c907c7c
unknown <elishamedenoukoume@gmail.com>
2025-01-27 02:03:59 -1100

    Fonction pour calculer la surface du pavé

--- paveMagique.dart
+++ paveMagique.dart
@@ -1,6 +1,6 @@
 void main() {
   print(calculVolumePave (5,6,7));
-   
+   print(calculSurfaceTotalePave(2.4,3,4.3)); 
 }
 
 
@@ -8,3 +8,6 @@ double calculVolumePave (double longueur, double largeur, double hauteur) {
   return longueur*largeur*hauteur;
   
   }
+  double calculSurfaceTotalePave (double longueur, double largeur, double hauteur){
+  
+  return 2*(longueur*largeur + longueur*hauteur + largeur*hauteur);
